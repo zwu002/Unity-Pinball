76f7eba901ca64b40322e356bc871de22cac3c05
zwu002 <zwu002@gold.ac.uk>
2017-02-13 16:42:26 +0000

    Animated buttons! A visual stimulation for players.

---
+++ Pinball/Assets/Scripts/PlayAnimation.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour {
+
+    public GameObject buttonBouncy;
+    public bool hitBall;
+    int timer;
+
+    Animator buttonAnimator;
+
+    Rigidbody2D rb;
+
+    void Start () {
+        timer = 0;
+        buttonAnimator = GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+
+	void Update () {
+
+
+        if (hitBall == true)
+        {
+            buttonAnimator.SetBool("playHit", hitBall);
+            timer++;
+        }
+
+        if (timer>=15)
+        {
+            hitBall = false;
+            buttonAnimator.SetBool("playHit", hitBall);
+            timer = 0;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            hitBall = true;
+        }
+    }
+}
+
