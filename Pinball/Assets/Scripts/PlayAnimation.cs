5d7948e06cb418d8e8d371f5f81db8c50d65a910
zwu002 <zwu002@gold.ac.uk>
2017-02-24 14:47:15 +0000

    combo bonus changed back to 150pts More force for flippers New artwork for the catcher

--- Pinball/Assets/Scripts/PlayAnimation.cs
+++ Pinball/Assets/Scripts/PlayAnimation.cs
@@ -45,7 +45,7 @@ public class PlayAnimation : MonoBehaviour {
             if (col.gameObject.GetComponent<Ball>().playCombo == true)                              // 'Playcombo' detection
             {
                 col.gameObject.GetComponent<Ball>().celebrate.Play();                               // Play celebration sound
-                col.gameObject.GetComponent<Ball>().ballScore += 200;                               // Attach bonus points
+                col.gameObject.GetComponent<Ball>().ballScore += 150;                               // Attach bonus points
                 combo.SetActive(true);                                                              // Play celebration sprite
                 col.gameObject.GetComponent<Ball>().playCombo = false;                              // Refresh combo status                                                              // Bug fixing: avoid continuous combos
                 uiManager.GetComponent<UIManager>().comboUpdate();
